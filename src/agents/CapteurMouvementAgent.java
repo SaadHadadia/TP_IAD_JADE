4d23327a8d63220442f6d99febcf27943c92593d
Saad Hadadia <saad.hadadia9@gmail.com>
2025-10-22 15:54:45 +0100

    capteur de mouvement

--- src/agents/CapteurMouvementAgent.java
+++ src/agents/CapteurMouvementAgent.java
@@ -1,4 +1,18 @@
 package agents;
 
-public class CapteurMouvementAgent {
+import jade.core.Agent;
+
+public class CapteurMouvementAgent extends Agent {
+
+    private int nombreDetections = 0;
+
+    protected void setup() {
+        // TODO: Ajouter un CyclicBehaviour ou TickerBihavior
+    }
+
+    // Méthode pour simulier la détection
+    private boolean detecterMouement() {
+        return Math.random() < 0.4; // 40% de chance
+    }
+
 }
