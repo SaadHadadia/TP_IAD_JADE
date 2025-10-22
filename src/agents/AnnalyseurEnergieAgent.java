3e35c4f187856c2480088a21b67e94cf1c6fd549
Saad Hadadia <saad.hadadia9@gmail.com>
2025-10-22 16:10:31 +0100

    analyseur d'énergie

--- src/agents/AnnalyseurEnergieAgent.java
+++ src/agents/AnnalyseurEnergieAgent.java
@@ -1,4 +1,27 @@
 package agents;
 
-public class AnnalyseurEnergieAgent {
+import jade.core.Agent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AnnalyseurEnergieAgent extends Agent {
+
+    private List<Double> historique = new ArrayList<>();
+    private final int TAILLE_HISTORIQUE = 5;
+    private int nombreAnalyses = 0;
+
+    protected void setup() {
+        // TODO: Ajouter un compteur d'analyse
+    }
+
+    private double calculerMoyenne() {
+        // TODO: calculer la moyenne des dernières mesures
+    }
+
+    private void suggererEconomies(double consomation) {
+        if (consomation > 150) {
+            System.out.println("[AnnalyseurEnergie] ⚠ RECOMENDATION: Réduire la consomation");
+        }
+    }
 }
